cc05fad48f84465b3d24ab868f8aedc0180eb6ab
to0ken <evgeniamafinova@gmail.com>
2026-09-15 11:32:10 +0300

    Create SQLdata_typ.sql

---
+++ scheme/SQLdata_typ.sql
@@ -0,0 +1,12 @@
+DECLARE @DT datetime = '2011-11-11 04:23:33'
+DECLARE @DT2 datetime = GetDate()
+DECLARE @D date = GetDate()
+DECLARE @T time = GetDate()
+
+PRINT @DT 
+PRINT @D
+PRINT @T
+
+SELECT @DT AS date_time, @D AS date_, @T AS time_
+UNION ALL
+SELECT @DT2 AS date_time, @D AS date_, @T AS time_
